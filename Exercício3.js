c2165ff8940ad445d33791a1749aeaf4ea7bdded
ktbra-dev <kgtbra@gmail.com>
2026-08-06 15:31:54 -0300

    feat: Exercício 4 - Apenas Maiores que 10

--- Exercício3.js
+++ Exercício3.js
@@ -1,4 +1,4 @@
 //Exercício 3 - Buscar Capital
 const capitais = ['Brasília', 'Salvador', 'Manaus', 'Recife'];
-let busca = capitais.find((capital) => capital == "Manaus");
-console.log(busca)
+let busca = capitais.find((capital) => capital == 'Manaus');
+console.log(busca);
---
+++ Exercício4.js
@@ -0,0 +1,7 @@
+//Exercício 4 - Apenas Maiores que 10
+const numeros = [5, 9, 12, 4, 18, 15];
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > 10) {
+    console.log(numeros[i])
+}
+}
